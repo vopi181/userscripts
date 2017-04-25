cc192dded3423339d28a3a493e0a3e4f9d1904e5
Dominic Pace <dompace181@gmail.com>
2017-04-25 18:20:38 -0500

    added download/update urls

--- scripts/g2a.com/g2a-auto-disable-shield.user.js
+++ scripts/g2a.com/g2a-auto-disable-shield.user.js
@@ -8,10 +8,13 @@
 // @include www.g2a.com*
 // @include *.g2a.com*
 // @homepage https://github.com/vopi181/userscripts
+// @updateURL https://github.com/vopi181/userscripts/blob/master/scripts/g2a.com/g2a-auto-disable-shield.user.js
+// @downloadURL https://github.com/vopi181/userscripts/blob/master/scripts/g2a.com/g2a-auto-disable-shield.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     disableEscrow();
     
+
 })();
