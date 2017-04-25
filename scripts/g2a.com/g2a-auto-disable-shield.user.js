ee8f92c2ec9329f422f9d887a4efa20762e175fd
Dominic Pace <dompace181@gmail.com>
2017-04-25 18:19:27 -0500

    first script

---
+++ scripts/g2a.com/g2a-auto-disable-shield.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         G2A Auto Disable shield
+// @namespace    https://dominicpace.me/
+// @version      0.1
+// @description  Auto disables shield. Tbh its pretty stupid but it works
+// @author       vopi181
+// @grant        none
+// @include www.g2a.com*
+// @include *.g2a.com*
+// @homepage https://github.com/vopi181/userscripts
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    disableEscrow();
+    
+})();
